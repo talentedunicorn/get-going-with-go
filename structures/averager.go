487e4f3e20361328ad7fbd07fe47b06ca8920665
Ezekiel Mangalu <emangalu@gmail.com>
2018-10-17 00:18:18 +0800

    Sometime later...

--- structures/averager.go
+++ structures/averager.go
@@ -2,28 +2,39 @@ package main
 
 import (
   "fmt"
-  // "bufio"
-  // "os"
+  "bufio"
+  "os"
+  "strings"
+  "strconv"
 )
 
 func main() {
-  // fmt.Println("Enter comma separated number to get average:")
-  // reader := bufio.NewReader(os.Stdin)
-  // input, _ := reader.ReadString('\n')
-  // fmt.Println("Input:", input)
+  fmt.Println("Enter comma separated number to get average:")
+  reader := bufio.NewReader(os.Stdin)
+  input, _ := reader.ReadString('\n')
 
-  numbers := []float64{2, 4, 5.5, 19}
+  inputMap := strings.Split(input, ",")
+  var numbers []float64
+  for _, n := range inputMap {
+    number, _ := strconv.ParseFloat(n, 64)
+    numbers = append(numbers, number)
+  }
+
+  // numbers := []float64{2, 4, 5.5, 19}
   avg := average(numbers)
 
   fmt.Println("Numbers", numbers)
   fmt.Println("Averages to:", avg)
 }
 
-func average(numbers []float64) float64 {
+func average(numbers []float64) (res float64) {
   var total float64
   for _, v := range numbers {
     total += v
   }
 
-  return total / float64(len(numbers))
+  res = total / float64(len(numbers))
+  return res
+  // Or
+  // return total / float64(len(numbers))
 }
